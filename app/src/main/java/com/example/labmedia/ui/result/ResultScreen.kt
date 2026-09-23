2bb96859ee56a8b2917de43de72e0f469a78456a
AdidAsik360 <sokolovava07@gmail.com>
2026-09-23 15:36:03 +0300

    Update ResultScreen.kt

--- app/src/main/java/com/example/labmedia/ui/result/ResultScreen.kt
+++ app/src/main/java/com/example/labmedia/ui/result/ResultScreen.kt
@@ -89,7 +89,6 @@ fun ResultScreen(
 
             Spacer(Modifier.height(20.dp))
 
-            // Карточка результата
             ResultCard(result = result)
 
             Spacer(Modifier.height(24.dp))
